d24234314f00161bd9852f8291c0234814bb8588
Ti0v <100233720+Ti0v@users.noreply.github.com>
2023-12-02 22:56:04 +0300

    fffd

--- src/main/java/com/CachWeb/Cach/config/WebConfig.java
+++ src/main/java/com/CachWeb/Cach/config/WebConfig.java
@@ -1,12 +1,14 @@
 package com.CachWeb.Cach.config;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 
 @Configuration
@@ -16,7 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/assets/**")
                 .addResourceLocations("classpath:/static/assets/");
     }
-
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        return templateResolver;
+    }
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/error/404").setViewName("error/404");
